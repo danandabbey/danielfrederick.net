6a9a5a6cf0bcb9a5e5ba8163e6f20cf7e5f5dcc3
Daniel Frederick <danandabbey@icloud.com>
2025-12-12 21:07:04 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Hello from './hello.tsx'
+
+function App() {
+  return (
+    <>
+      <div>
+        <Hello/>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/hello.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function Hello() {
+  return (
+    <>
+      <div >
+        <h1>Hello, my name is Daniel Frederick and this is my website</h1>
+      </div>
+    </>
+  )
+}
+
+export default Hello
