ee788886e98937c0726dfc1fb9307cf5fc25592a
Daniel Frederick <danandabbey@icloud.com>
2025-12-12 23:48:16 -0500

    updated hello.tsx

--- src/hello.tsx
+++ src/hello.tsx
@@ -4,7 +4,9 @@ function Hello() {
   return (
     <>
       <div >
-        <h1>Hello, my name is Daniel Frederick and this is my website</h1>
+        <h1>Hello, my name is</h1>
+        <h1>Daniel Frederick</h1>
+        <h1>and this is my website</h1>
       </div>
     </>
   )
