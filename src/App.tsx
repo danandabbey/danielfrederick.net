0fd45d7c04e75ef1ba4bfee82a71368bc1b38d35
Daniel Frederick <danandabbey@icloud.com>
2025-12-14 15:49:25 -0500

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
-import { useState } from 'react'
 import './App.css'
 import Hello from './components/hello.tsx'
 import Websites from './components/websites.tsx'
 
 function App() {
-  
-  const [view, setView] = useState(<Hello/>)
 
   return (
     <>
-      <div onClick={() => setView(<Websites/>)}>
-       {view}
+      <div >
+        <Hello/>
+        <Websites/>
       </div>
     </>
   )
